d0dfd3adee52e2ef275f7d3b8b8efe7f6065bad0
Quinten Simmons <Quinten_Simmons@Quintens-iMac.local>
2017-02-01 19:39:07 -0500

    Added Pokemon class in model folder and began styling with background, nav bar, search bar, and collection view.

---
+++ pokedexSwift3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  pokedexSwift3
+//
+//  Created by Quinten Simmons on 2/1/17.
+//  Copyright © 2017 Quinten Simmons. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
