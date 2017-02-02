b2b18814b3ee6a73f0651b8c273468c78fa0e697
Quinten Simmons <Quinten_Simmons@Quintens-iMac.local>
2017-02-02 11:50:35 -0500

    Added Collection view protocol methods, setup dequeuereusablecell, rounded corners of cells in pokecell, set datasource and delegate.

--- pokedexSwift3/ViewController.swift
+++ pokedexSwift3/ViewController.swift
@@ -8,15 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        collection.dataSource = self
+        collection.delegate = self
     }
 
     
-
+    //dequeues cells and sets them up
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            let pokemon = Pokemon(name: "Pokemon", pokedexId: (indexPath.row + 1))
+            cell.configureCell(pokemon: pokemon)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    //code in this function will execute when a cell is tapped
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    //sets number of items in the section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+    
+    //number of sections in the collectionview
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    //define the size of the cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 105, height: 105)
+    }
 
 }
 
