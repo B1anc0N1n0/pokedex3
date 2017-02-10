4c4731947af034f10451801cfd42405de3872e23
Quinten Simmons <Quinten_Simmons@Quintens-iMac.local>
2017-02-10 13:30:45 -0500

    Added Alamofire cocoapod Added Alamofire cocoapod to project for HTTP requests.

---
+++ Pods/Target Support Files/Pods-pokedexSwift3/Pods-pokedexSwift3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokedexSwift3 : NSObject
+@end
+@implementation PodsDummy_Pods_pokedexSwift3
+@end
--- pokedexSwift3/PokemonDetailVC.swift
+++ pokedexSwift3/PokemonDetailVC.swift
@@ -13,6 +13,17 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var discriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UIStackView!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,16 @@ class PokemonDetailVC: UIViewController {
         nameLbl.text = pokemon.name
         
     }
+    
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+
 
     
 
